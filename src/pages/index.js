33f4320a88644de0d5b65b47e3dc5db6bdc63062
Daniel <danielirwin99@gmail.com>
2023-01-30 13:14:06 +1100

    Finished Main Part

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import Header from "components/Header";
+import Nav from "components/Nav";
+import Results from "components/Results";
 import Head from "next/head";
+import requests from "utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
+  console.log(results);
   return (
     <>
       <Head>
@@ -12,8 +16,25 @@ export default function Home() {
       <Header />
 
       {/* NAV */}
-
+      <Nav />
       {/* RESULTS */}
+      <Results results={results} />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchPopular.url
+    }`
+  ).then((response) => response.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
---
+++ utils/requests.js
@@ -0,0 +1,44 @@
+const API_KEY = process.env.API_KEY;
+
+export default {
+  fetchPopular: {
+    title: "Popular",
+    url: `/movie/popular?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  },
+  fetchComedyMovies: {
+    title: "Comedy",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  },
+  fetchRomanceMovies: {
+    title: "Romance",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  },
+  fetchMystery: {
+    title: "Mystery",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  },
+  fetchSciFi: {
+    title: "Sci-Fi",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  },
+  fetchWestern: {
+    title: "Western",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  },
+  fetchAnimation: {
+    title: "Animation",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  },
+  fetchTV: {
+    title: "Tv Movie",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+  },
+};
